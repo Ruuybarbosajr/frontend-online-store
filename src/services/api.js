c28c0f9cf311f3a4c70b5c48208105d38a24a8fb
Rafael Amaral <amaral.avelar.filo@gmail.com>
2022-01-20 09:54:03 -0300

    erro no fetch

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 export async function getCategories() {
   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
     .then((response) => response.json())
-    .then((data) => data);
+    .then((data) => data).catch(error => console.log(error))
+
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
